3d7c453c5b43744fe494e53e466b88a9c5ef0fd2
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2024-11-02 17:54:28 +0600

    schema

--- store/models.py
+++ store/models.py
@@ -13,9 +13,10 @@ class Promotion(models.Model):
 
 class Product(models.Model):
     sku = models.CharField(max_length=10, primary_key=True)
+    slug = models.SlugField()
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -37,7 +38,6 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default= MEMBERSHIP_BRONZE)
-
 
 
 class Order(models.Model):
@@ -62,6 +62,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255, default=None)
     #One-to-One relation
     #customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
     #One-to-many
